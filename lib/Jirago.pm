ab790e764adf7e1344c71a8a69176380bb57e0e4
Jay Shirley <j@shirley.im>
2010-09-22 09:57:30 -0700

    Makefile and readmes

---
+++ lib/Jirago.pm
@@ -0,0 +1,31 @@
+package Jirago;
+
+=head1 NAME
+
+Jirago - Where's Jira going?
+
+=head1 DESCRIPTION
+
+While Jira is an excellent and full-featured bug tracking and reporting system,
+it does lack a lot when it comes to figuring out the status of an upcoming
+release. They have plenty of tools, and an API but we found that insufficient.
+Also, ugly.
+
+Jirago is a simple application inspired by the Panic Board
+(L<http://www.panic.com/blog/2010/03/the-panic-status-board/>) to show us
+exactly where we are going, how far away we are and what we need to do.
+
+=head1 AUTHOR
+
+Jay Shirley <jshirley@magazines.com>
+
+=head1 LICENSE & COPYRIGHT
+
+Copyright (c) 2010 by the aforementioned authors.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+
